b50f6fd70bc822fa20bf21dde87de72b2f18df7f
Christian Engel <engel.christian1994@gmail.com>
2024-08-01 21:25:26 -0400

    Add worker_heartbeat_rejects_unknown_node

--- crates/melond/tests/scheduler/worker_tests.rs
+++ crates/melond/tests/scheduler/worker_tests.rs
@@ -16,3 +16,11 @@ async fn worker_heartbeat_works() {
     let res = app.send_heartbeat(node_id).await;
     assert!(res.is_ok())
 }
+
+#[tokio::test]
+async fn worker_heartbeat_rejects_unknown_node() {
+    let app = spawn_app().await;
+    let node_id = String::from("UNKNOWN");
+    let res = app.send_heartbeat(node_id).await;
+    assert!(res.is_err())
+}
