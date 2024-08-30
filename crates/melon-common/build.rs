58ec5b58c8fc484b1270d9fc3d90807b43ac619e
Christian Engel <engel.christian1994@gmail.com>
2024-08-30 17:51:25 -0400

    Fix cgroups path

--- crates/melon-common/build.rs
+++ crates/melon-common/build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../../proto/melon.proto")?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["../../proto/melon.proto"], &["../../proto"])?;
     Ok(())
 }
--- install-all.sh
+++ install-all.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 set -e
 
-# ensure permission to write to /usr/local/bin
-if [ ! -w /usr/local/bin ]; then
-    echo "Error: You don't have write permission to /usr/local/bin"
-    echo "Try running this script with sudo"
-    exit 1
-fi
-
-# Build and install each crate
 for crate in melond mbatch mworker mqueue mcancel mextend mshow; do
     echo "Building $crate..."
     cargo build --release --manifest-path crates/$crate/Cargo.toml
+done
 
+echo "Installing binaries to /usr/local/bin. You may be prompted for your password."
+for crate in melond mbatch mworker mqueue mcancel mextend mshow; do
     echo "Installing $crate to /usr/local/bin..."
-    cp target/release/$crate /usr/local/bin/
-    chmod +x /usr/local/bin/$crate
+    sudo cp target/release/$crate /usr/local/bin/
+    sudo chmod +x /usr/local/bin/$crate
 done
 
 echo "All binaries installed successfully in /usr/local/bin!"
