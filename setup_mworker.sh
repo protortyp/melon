0f98f336a9f88990645fa80a045e05026abd00c7
Christian Engel <engel.christian1994@gmail.com>
2024-08-30 22:15:21 -0400

    Final fix

--- setup_mworker.sh
+++ setup_mworker.sh
@@ -6,7 +6,7 @@ sudo adduser mworker --no-create-home --disabled-login
 # set up cgroup delegation
 sudo mkdir /sys/fs/cgroup/melon
 sudo chown -R mworker:mworker /sys/fs/cgroup/melon
-echo "+cpu +cpuset +memory +io" | sudo tee /sys/fs/cgroup/melon/cgroup.subtree_control
+echo "+cpuset +memory +io" | sudo tee /sys/fs/cgroup/melon/cgroup.subtree_control
 
 # set sudo permissions
 echo "mworker ALL=(root) NOPASSWD: /bin/echo [0-9]* > /sys/fs/cgroup/melon/*/cgroup.procs" | sudo EDITOR='tee -a' visudo
