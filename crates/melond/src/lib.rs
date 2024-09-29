112bb6152f5d4dc6d80c33a877d38ab2b4116cfe
Christian Engel <engel.christian1994@gmail.com>
2024-09-28 21:11:29 -0400

    Re-export stuff in melond

--- crates/melond/src/lib.rs
+++ crates/melond/src/lib.rs
@@ -1,5 +1,13 @@
 pub mod api;
 pub mod application;
 pub mod db;
+pub mod error;
 pub mod scheduler;
 pub mod settings;
+
+// re-export
+pub use api::Api;
+pub use application::Application;
+pub use error::Result;
+pub use scheduler::Scheduler;
+pub use settings::Settings;
